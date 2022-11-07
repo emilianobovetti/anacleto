813239985d9fc6313bb522a6d728435b9346f692
Emiliano <emiliano.bovetti@prima.it>
2022-11-07 21:28:50 +0100

    chore: pilade local env (#4)

--- pilade/entrypoint.sh
+++ pilade/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
+INDEX=/home/app/pilade/src/index.js
 APP_ENV="HOME=/home/app $(env | grep '^PILADE_' | tr '\n' ' ')"
 
-echo "00 04 * * * export $APP_ENV; node /home/app/src/index.js >>/var/log/pilade.log 2>&1" | crontab -
+echo "00 04 * * * export $APP_ENV; node $INDEX >>/var/log/pilade.log 2>&1" | crontab -
 
 touch /var/log/pilade.log
 chown app:app /var/log/pilade.log
