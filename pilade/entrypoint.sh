751d6cb7f94ffabfebe0b2fc30f05495222db39e
Emiliano Bovetti <emiliano.bovetti@prima.it>
2022-11-13 00:50:36 +0100

    feat(pilade): persistent logs

--- pilade/entrypoint.sh
+++ pilade/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env sh
 
-INDEX=/home/app/pilade/src/index.js
 APP_ENV="HOME=/home/app $(env | grep '^PILADE_' | tr '\n' ' ')"
+JS_ENTRYPOINT=/home/app/pilade/src/index.js
+LOG_DIR=/var/log/pilade
+LOG_FILE="$LOG_DIR/pilade.log"
 
-echo "00 04 * * * export $APP_ENV; node $INDEX >>/var/log/pilade.log 2>&1" | crontab -
+mkdir -p "$LOG_DIR"
+touch "$LOG_FILE"
+chown -R app:app "$LOG_DIR"
 
-touch /var/log/pilade.log
-chown app:app /var/log/pilade.log
-tail -f /var/log/pilade.log &
+echo "00 04 * * * export $APP_ENV; node $JS_ENTRYPOINT >>$LOG_FILE 2>&1" | crontab -
+
+tail -f "$LOG_FILE" &
 
 exec cron -f -L 7
